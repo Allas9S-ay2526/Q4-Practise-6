966e4948239c064fff18fd0ec5713570ac25b673
Allas-9S-ay2526 <allas.ac@obmontessori.edu.ph>
2026-04-28 07:48:30 +0800

    submit to main

---
+++ main.js
@@ -0,0 +1,7 @@
+function countANimals() {
+    var animals = ["Dog", "Cat", "Rat", "Boa Constrictor"]
+    for (var i = 0; i<animals.length; i++) {
+        window.alert("This zoo has a " + animals [i] + ".");
+    }
+
+}
